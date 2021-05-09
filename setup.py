1cc6df4a66b0c2b8a799d5c3a5cde3f2166f645f
Frank Yang <puilp0502@gmail.com>
2021-05-10 01:00:14 +0900

    fix: setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,27 @@
-from setuptools import Extension, setup
+from setuptools import Extension, setup, find_packages
 from Cython.Build import cythonize
 
+VERSION = "0.0.1"
+FZY_VERSION = "1.0"
+
+include_path = ["src/fzy/"]
 extensions = [
-    Extension("fzy.choices", ["src/choices.pyx"], define_macros=[("VERSION", '"1.0"')]),
-    Extension("fzy.match", ["src/match.pyx"], define_macros=[("VERSION", '"1.0"')])
+    Extension(
+        "fzy.choices",
+        ["src/fzy/choices.pyx"],
+        define_macros=[("VERSION", '"{}"'.format(FZY_VERSION))],
+    ),
+    Extension(
+        "fzy.match",
+        ["src/fzy/match.pyx"],
+        define_macros=[("VERSION", '"{}"'.format(FZY_VERSION))],
+    ),
 ]
-setup(ext_modules=cythonize(extensions))
+
+setup(
+    name="fzy",
+    version="{}-{}".format(VERSION, FZY_VERSION),
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    ext_modules=cythonize(extensions, include_path=include_path),
+)
---
+++ src/fzy/__init__.py
@@ -0,0 +1,11 @@
+from fzy.choices import Choice
+from fzy.search import search
+from fzy.match import match_bytes, match
+
+
+__all__ = [
+    "search",
+    "Choice",
+    "match_bytes",
+    "match",
+]
