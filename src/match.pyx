dfeda5252e8f1645e6372cad838409984b8807bf
Frank Yang <puilp0502@gmail.com>
2021-05-09 23:27:14 +0900

    chore: let match.match accept str

--- src/match.pyx
+++ src/match.pyx
@@ -5,8 +5,15 @@ cimport fzy_match
 import math
 
 
-def match(const char* search, const char* candidate):
+def match_bytes(const char* search, const char* candidate):
     if fzy_match.has_match(search, candidate):
         return fzy_match.match(search, candidate)
     else:
         return -1* math.inf
+
+def match(search, candidate):
+    if not isinstance(search, str) or not isinstance(candidate, str):
+        raise TypeError(
+            "expected (str, str), got ({}, {})".format(type(search), type(candidate))
+        )
+    return match_bytes(search.encode("utf-8"), candidate.encode("utf-8"))
